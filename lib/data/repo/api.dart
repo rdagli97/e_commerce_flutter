3f35eb5864cfb9abc9f3cc29250c3a6bcb0a9833
Ramazan Dagli <rdagliflutter@gmail.com>
2023-09-19 09:10:45 +0300

    services

--- lib/data/repo/api.dart
+++ lib/data/repo/api.dart
@@ -1,7 +1,13 @@
 import 'dart:convert' as convert;
 import 'dart:developer' as developer;
 
+import 'package:flutter_e_commerce_app/data/models/buyed_products.dart';
+import 'package:flutter_e_commerce_app/data/models/comment_image_model.dart';
+import 'package:flutter_e_commerce_app/data/models/comment_model.dart';
+import 'package:flutter_e_commerce_app/data/models/product_image_model.dart';
+import 'package:flutter_e_commerce_app/data/models/product_model.dart';
 import 'package:flutter_e_commerce_app/data/models/user_model.dart';
+import 'package:flutter_e_commerce_app/data/repo/shared_preference.dart';
 import 'package:flutter_e_commerce_app/resources/consts/service_strings.dart';
 import 'package:http/http.dart' as http;
 
@@ -105,11 +111,11 @@ class API {
       final response = await http.post(
         Uri.parse('$baseUrl/login'),
         headers: {
-          'Accept' : applicationJson,
+          'Accept': applicationJson,
         },
         body: {
-          'email' : email,
-          'password' : password,
+          'email': email,
+          'password': password,
         },
       );
 
@@ -133,10 +139,505 @@ class API {
 
   Future<ApiResponse> updateUser({
     required String image,
-    required String address,
+    required String adress,
     required String aboutUs,
   }) async {
     ApiResponse apiResponse = ApiResponse();
-    
+    String? token;
+    token = await SharedPreference().getToken();
+
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/update'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {'image': image, 'adress': adress, 'about_us': aboutUs},
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = UserModel.fromJson(body);
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  // usermodel need to be fixed
+  /// Products with images and comments
+  /// buyedProducts
+  /// favourites
+  /// saledProducts
+
+  Future<ApiResponse> getCurrentUserDetails() async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == 'null' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/user'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = UserModel.fromJson(body);
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  // PRODUCT
+
+  Future<ApiResponse> createProduct({
+    required String title,
+    required String description,
+    required double price,
+    required String category,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/product/create'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'title': title,
+          'description': description,
+          'price': price,
+          'category': category,
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = ProductsModel.fromJson(body);
+          break;
+        case 422:
+          final errors = body['errors'];
+          apiResponse.error = errors[errors.keys.elementAt(0)][0];
+          developer.log(apiResponse.error.toString());
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  // delete product
+  Future<ApiResponse> deleteProduct(int productId) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.delete(
+        Uri.parse('$baseUrl/$productId/delete'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = body['message'];
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> giveDiscount(int productId, int discountValue) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/$productId/discount'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'discount': discountValue,
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = body['message'];
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> getProducts() async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/products'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          List<dynamic> productList = List.from(body['products']);
+          apiResponse.data =
+              productList.map((e) => ProductsModel.fromJson(e)).toList();
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  // buy product
+  Future<ApiResponse> buyProduct({
+    required int productId,
+    required int piece,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/buyed_product/$productId/create'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'piece': piece,
+        },
+      );
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = BuyedProductsModel.fromJson(body['buyedProduct']);
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  // create comment
+  Future<ApiResponse> createComment({
+    required int rating,
+    required String comment,
+    required String productId,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/product/$productId/create'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'rating': rating,
+          'comment': comment,
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = CommentsModel.fromJson(body['comment']);
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> deleteComment({
+    required int commentId,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.delete(
+        Uri.parse('$baseUrl/comments/$commentId/delete'),
+        headers: {'Accept': applicationJson, 'Authorization': 'Bearer $token'},
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = body['message'];
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> addOrRemoveFavourite({
+    required int productId,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/product/$productId/favourite'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = body['message'];
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> createProductImage({
+    required String image,
+    required String productId,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/product/$productId/create/product_image'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'image': image,
+        },
+      );
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = ProductImageModel.fromJson(body['productImage']);
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+          break;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
+  }
+
+  Future<ApiResponse> createCommentImage({
+    required String image,
+    required int commentId,
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+    String? token = await SharedPreference().getToken();
+    if (token == '' || token.isEmpty) {
+      developer.log(nullToken);
+    }
+
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/comment/$commentId/create/comment_image'),
+        headers: {
+          'Accept': applicationJson,
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'image': image,
+        },
+      );
+      final dynamic body = convert.jsonDecode(response.body);
+      switch (response.statusCode) {
+        case 200:
+          apiResponse.data = CommentImage.fromJson(body['commentImage']);
+          break;
+        case 404:
+          apiResponse.error = body['message'];
+          break;
+        case 403:
+          apiResponse.error = body['message'];
+          break;
+        case 401:
+          apiResponse.error = unauthorized;
+          break;
+        default:
+          apiResponse.error = somethingWentWrong;
+      }
+    } catch (e) {
+      apiResponse.error = serverError;
+    }
+    return apiResponse;
   }
 }
